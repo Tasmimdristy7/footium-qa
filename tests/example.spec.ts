c52b8a82347922e8b83bcd34223162d7406b174d
Jasper Dansercoer <jasper@codictive.be>
2023-03-20 15:38:18 +0100

    Added readme

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("YOUR TEST HERE", async ({ page }) => {
+  // Your test here
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  expect(true).toBe(true);
 });
