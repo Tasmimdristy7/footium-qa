454cc8804e6aedee2429e17f7a7bff26033ee00d
tasmimdristy7 <tasmimdristy7@gmail.com>
2023-04-17 20:44:35 +0600

    QA Task

---
+++ tests/README.md
@@ -0,0 +1,26 @@
+Here is a summary of what the test does:
+* It visits the Footium website and clicks the "View Clubs on OpenSea" button.
+* It verifies that the button links to the correct URL on OpenSea.
+* It navigates to the Footium website again and verifies that the "Our Investors" heading is present on the page.
+* It checks that all the listed investors are present on the page.
+* It checks that all the listed GamingDao are present on the page.
+* It clicks the "Play" button in the top right corner of the page.
+* It checks that the competition table shows 12 clubs, each with its own logo.
+
+Each test step is wrapped in a Playwright test function and uses Playwright's API to interact with the page and perform checks. The expect function is used to assert that certain conditions are true, and the console.log part is used to output information to the console for debugging purposes.
+
+
+Although I went through the Playwright documentation, and tutorials and wrote this code in just one day, It does have some similarities with Cypress. I admit my code is not optimized to its fullest potential. If I had more time, I would have liked to learn more about efficient ways to handle pages and timeouts in Playwright, in order to improve the code and make it more reliable.
+
+** On line 75 you might hit a time-out error just look out for that. **
+
+
+
+
+
+Things I would have done better if it was another framework that I am used to: 
+
+- Timeout optimization
+- Handling multiple pages
+- Organize it with a POM.
+
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,113 @@
 import { test, expect } from "@playwright/test";
 
-test("YOUR TEST HERE", async ({ page }) => {
-  // Your test here
+test.describe("Footium-navigation", () => {
+  test.beforeEach(async ({ page }) => {
+    // Visits Footium website.
+    await page.goto("https://footium.club");
+    
+    const page1Promise = page.waitForEvent('popup');
+    //Clicks on the Open sea button as medium is banned i tried this one
+    
+    await page.click('button:has-text("View Clubs on OpenSea")');
+    const page1 = await page1Promise;
+    await page1.close();
+//Checks that if the button links to the correct
+    expect(page1.url()).toBe('https://opensea.io/collection/footium-football-clubs');
+    console.log(page1.url())
+    expect(true).toBe(true);
+  });
 
-  expect(true).toBe(true);
+  test("Footium test 2", async ({page}) => {
+   //Verify the Investors
+    await page.goto("https://footium.club");
+    
+    
+      const heading = page.getByRole('heading', { name: 'Our Investors' });
+      const headingText = 'Our Investors';
+      const timeout = 5000;
+  
+      await expect(heading).toContainText(headingText);
+    
+      console.log(await heading.textContent());
+      
+      //Here checks all the investors exists or not, I used array for quick test but there are some better ways also
+      
+      const investors = ['Animoca Brands', 'Stride VC', 'Backed VC', 'Entreé Capital', 'Concept Ventures', 'IVC', 'Encode Club','Chris Smalling','Michael Bentley','Ahmed Al-Balaghi','SolBigBrain'];
+     
+      let allInvestorsFound = true;
+      
+      for (const investor of investors) {
+          const investorSelector = `text=${investor}`;
+          await page.waitForSelector(investorSelector);
+          const investorElement = await page.$(investorSelector);
+          if (!investorElement) {
+            allInvestorsFound = false;
+            console.log(`Investor "${investor}" not found on page.`);
+          }
+       
+          expect(investorElement).toBeTruthy();
+        }
+        if (allInvestorsFound) {
+          console.log('All investors and angle investor found on page.');
+        } else {
+          console.log('Verification failed: some investors not found on page.');
+        }
+//Checks if all the GamingDao exists or not
+        const GamingDao = ['Merit Circle','BlackPool','BAYZ','Yield Guild Games SEA']
+        let allGamingDao = true;
+        for (const Dao of GamingDao){
+          const GamingDaoSelector = `text=${Dao}`;
+          await page.waitForSelector(GamingDaoSelector);
+          const GamingDaoElement = await page.$(GamingDaoSelector);
+          if (!GamingDaoElement) {
+            allGamingDao = false;
+            console.log(`Gaming Dao "${Dao}" not found on page.`);
+          }
+          expect(GamingDaoElement).toBeTruthy();
+          }
+          if (allGamingDao) {
+            console.log('All gamingDao found on page.');
+          } else {
+            console.log('Verification failed: some  not found on page.');
+          }
+
+        
+          //Clicks the PLAY button in the top right corner
+          await page.getByRole('button', { name: 'Play' }).click();
+
+          //Ensures that the Competition table shows 12 clubs, each with their own logo
+          //Locates the compettition table here
+              const tableSelector = ".css-10ll2cr";
+              await page.waitForSelector(tableSelector);
+
+              // selecting all rows within a table
+              const rows = await page.$$(tableSelector + " tbody tr");
+
+              //checks the rows 
+              expect(rows.length).toBe(12);
+              console.log(rows.length)
+
+              // Check each row for the logo image
+              
+              for (const row of rows) {
+                const logoSelector = "img";
+                const logoElement = await row.$(logoSelector);
+                expect(logoElement).toBeTruthy();
+              
+                if (logoElement) {
+                  console.log("Logo is present for row");
+                  expect(true).toBe(true); // pass condition
+                } else {
+                  console.log("Logo is not present for row");
+                  expect(false).toBe(true); // fail condition
+                }
+              }
+              
+              expect(true).toBe(true);
+  });
 });
+
+
+
+
+  
